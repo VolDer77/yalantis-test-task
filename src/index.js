62cd553e11ff485daeae11b8ff26ecdafc3385d6
Volodymyr Derkach <56925437+VolDer77@users.noreply.github.com>
2021-05-22 13:48:22 +0300

    add routing

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Route path="/employees">
+        <App />
+      </Route>
+      <Redirect to="/employees" />
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
